24c9bde2727f59510e615b713dda4bf16b9921c3
igordbauer <igordbauer@gmail.com>
2024-03-23 00:25:50 -0300

    11

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  let count = 0;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<h1>Counter</h1>
+
+<div>
+  <button on:click={() => (count = count + 1)}>Inc</button>
+  <p>{count}</p>
+  <button on:click={() => (count = count - 1)}>Dec</button>
+</div>
+<div>
+  <button on:click={() => (count = count + 1)}>Inc</button>
+  <p>{count}</p>
+  <button on:click={() => (count = count - 1)}>Dec</button>
+</div>
+
+<style>
+  div {
+    display: flex;
+  }
+  button {
+    margin: 10px;
+  }
+</style>
